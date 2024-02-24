1aba2b07b629135a54480cb6b5551d61daad6d71
vamshigithubcollab <d.mohanavamshi1105@gmail.com>
2024-02-24 22:53:56 +0530

    database changed

--- server/database.sql
+++ server/database.sql
@@ -5,5 +5,6 @@ create table authorise(
     id uuid PRIMARY KEY default uuid_generate_v4(),
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
-    user_role VARCHAR(255) default "USER"
+    user_role VARCHAR(255) default "USER",
+    user_password VARCHAR(255) NOT NULL
 );
